aafa28256849082b4c6cb780d1211892e2c84d8d
Tommy Brits <tommybrits74@gmail.com>
2023-11-22 06:27:29 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const current = new Date.now();
+const backThen = new Date('December 12 21 23:00:00:00')
+const domContainer = document.querySelector(".time-container");
+
+
+console.log(current)
+console.log(backThen)
